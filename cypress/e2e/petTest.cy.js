785e409c5ca40ae4e247019ff9b60b90efc6c706
Kovaliv Ivanna <ivanna.kovaliv1215@gmail.com>
2024-06-29 12:57:55 +0300

    Updated test

--- cypress/e2e/petTest.cy.js
+++ cypress/e2e/petTest.cy.js
@@ -1,19 +1,11 @@
-import { faker } from '@faker-js/faker';
-import pet from '../fixtures/pet.json';
 
-pet.name = faker.animal.dog();
-pet.category.name = faker.animal.type();
-// pet.category.id = faker.number.int({ min: 9000, max: 900000 })
-pet.id = faker.number.int({ min: 9000, max: 900000 })
+import petTemplate from '../fixtures/pet.json';
+import { generateRandomPet } from '../support/helper';
 
-pet.tags[0].name = faker.animal.bear();
-pet.tags[0].id = faker.number.int({ min: 9000, max: 900000 })
-pet.tags[1] = pet.tags[0]
-pet.tags[1].name = faker.animal.cat();
-pet.tags[1].id = faker.number.int({ min: 9000, max: 900000 })
-
+let pet = generateRandomPet(petTemplate, true, true, true, true, false);
+let petId;
 describe('PET test suite', () => {
-  let petId;
+
   it('Create pet', () => {
     cy.log('Create pet')
     cy.request('POST', '/pet', pet).then((response) => {
@@ -23,7 +15,10 @@ describe('PET test suite', () => {
       expect(response.body.category.id).to.eq(pet.category.id);
       expect(response.body.category.name).to.eq(pet.category.name);
       expect(response.body.tags[0].id).to.eq(pet.tags[0].id);
-      expect(response.body.tags[1].id).to.eq(pet.tags[1].id);
+      expect(response.body.tags[0].name).to.eq(pet.tags[0].name);
+      expect(response.body.tags[1].id).to.eq(pet.tags[1].id)
+      expect(response.body.tags[1].name).to.eq(pet.tags[1].name);
+      expect(response.body.status).to.eq(pet.status);
       console.log(response)
 
       // cy.log(`${JSON.stringify(response.body)}`)
@@ -38,15 +33,56 @@ describe('PET test suite', () => {
         expect(response.body.category.id).to.eq(pet.category.id);
         expect(response.body.category.name).to.eq(pet.category.name);
         expect(response.body.tags[0].id).to.eq(pet.tags[0].id);
+        expect(response.body.tags[0].name).to.eq(pet.tags[0].name);
         expect(response.body.tags[1].id).to.eq(pet.tags[1].id)
+        expect(response.body.tags[1].name).to.eq(pet.tags[1].name);
+        expect(response.body.status).to.eq(pet.status);
       })
 
     })
 
-
   })
 
+})
+
+describe('PET test suite', () => {
+
+  pet = generateRandomPet(petTemplate, false, true, true, true, false);
+
+  it('Add a new pet to the store', () => { })
+
+  it('Update pet', () => {
+    cy.log('Update pet');
+
+    cy.request('PUT', '/pet', pet).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.name).to.eq(pet.name);
+      expect(response.body.category.id).to.eq(pet.category.id);
+      expect(response.body.category.name).to.eq(pet.category.name);
+      expect(response.body.tags[0].id).to.eq(pet.tags[0].id);
+      expect(response.body.tags[0].name).to.eq(pet.tags[0].name);
+      expect(response.body.tags[1].id).to.eq(pet.tags[1].id)
+      expect(response.body.tags[1].name).to.eq(pet.tags[1].name);
+      expect(response.body.status).to.eq(pet.status);
+
+      // cy.log(petId)
+      cy.log('Get pet by id and verify pet created');
+      cy.request('GET', `/pet/${petId}`).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.name).to.eq(pet.name);
+        expect(response.body.category.id).to.eq(pet.category.id);
+        expect(response.body.category.name).to.eq(pet.category.name);
+        expect(response.body.tags[0].id).to.eq(pet.tags[0].id);
+        expect(response.body.tags[0].name).to.eq(pet.tags[0].name);
+        expect(response.body.tags[1].id).to.eq(pet.tags[1].id)
+        expect(response.body.tags[1].name).to.eq(pet.tags[1].name);
+        expect(response.body.status).to.eq(pet.status);
 
 
+      })
+
+    })
+
+  })
 
 })
