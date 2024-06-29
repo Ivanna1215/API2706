d89ecba35a9ab6cbf804a700f543912d78b5cf41
Kovaliv Ivanna <ivanna.kovaliv1215@gmail.com>
2024-06-29 10:52:20 +0300

    Added assetion into test

--- cypress/e2e/petTest.cy.js
+++ cypress/e2e/petTest.cy.js
@@ -14,8 +14,20 @@ describe('PET test suite', () => {
   it('Create pet', () => {
     cy.log('Create pet')
     cy.log(`Pet:${JSON.stringify(pet)}`)
-    cy.request('POST', '/pet', pet)
+    cy.request('POST', '/pet', pet).then((response) => {
+      console.log(response);
+      expect(response.status).to.eq(200);
+      expect(response.body.name).to.eq(pet.name);
+      expect(response.body.category.id).to.eq(pet.category.id);
+      expect(response.body.category.name).to.eq(pet.category.name);
+      expect(response.body.tags[0].id).to.eq(pet.tags[0].id);
+      expect(response.body.tags[1].id).to.eq(pet.tags[1].id)
+
+
+    })
   })
+
+
 
 
 })
